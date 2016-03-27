3ccc73a01ffcb33ac4dd1b27dd23884e2f1a0c0a
yanqirenshi <yanqirenshi@gmail.com>
2016-03-27 18:43:08 +0900

    split file

---
+++ src/managed-pool/index.lisp
@@ -0,0 +1,77 @@
+(in-package :upanishad)
+
+(defun class-%id-indexp (symbol)
+  (cl-ppcre:scan "^(\\S)+-%ID-INDEX$"
+                 (symbol-name symbol)))
+
+(defun get-objects-slot-index-name (class &optional (slot '%id))
+  "Return the keyword symbol naming the specified index of instances of class."
+  (let ((classname (if (symbolp class) (string class) (class-name class)))
+        (slotname  (symbol-name slot)))
+    (intern (concatenate 'string classname "-" slotname "-INDEX") :keyword)))
+
+(defun %add-object-to-slot-index (index object slot)
+  (let ((%id-map (gethash (slot-value object slot) index))
+        (%id     (%id object)))
+    (when (null %id-map)
+      (setf %id-map   (make-hash-table))
+      (setf (gethash (slot-value object slot) index) %id-map))
+    (unless (gethash %id %id-map)
+      (setf (gethash %id %id-map) (%id object)))))
+
+(defmethod add-object-to-slot-index ((pool pool) class slot object)
+  (let* ((index-name (get-objects-slot-index-name class slot))
+         (index (get-root-object pool index-name)))
+    (when (and index  (slot-boundp object slot))
+      (%add-object-to-slot-index index object slot))))
+
+(defun make-index ()
+  (make-hash-table :test test))
+
+(defmethod tx-create-objects-slot-index ((pool pool) class slot &optional (test #'equalp))
+  (let ((index-name (get-objects-slot-index-name class slot)))
+    (unless (get-root-object pool index-name)
+      (let ((index (make-index)))
+        (setf (get-root-object pool index-name) index)
+        (dolist (object (find-all-objects pool class))
+          (add-object-to-slot-index pool class slot object))))))
+
+(defmethod index-on ((pool pool) class &optional slots (test 'equalp))
+  (dolist (slot slots)
+    (execute-transaction
+     (tx-create-objects-slot-index pool class slot test))))
+
+(defun %remove-object-from-slot-index (index object slot)
+  (let ((%id-map (gethash (slot-value object slot) index))
+        (%id     (%id object)))
+    (when %id-map
+      (when (gethash %id %id-map)
+        (remhash %id %id-map))
+      (when (= (hash-table-size %id-map) 0)
+        (remhash (slot-value object slot) index)))))
+
+(defmethod remove-object-from-slot-index ((pool pool) class slot object)
+  (let* ((index-name (get-objects-slot-index-name class slot))
+         (index (get-root-object pool index-name)))
+    (when (and index (slot-boundp object slot))
+      (%remove-object-from-slot-index index object slot))))
+
+(defmethod tx-remove-object-on-slot-index ((pool pool)
+                                           (obj  meme)
+                                           (slot-symbol symbol))
+  (let* ((obj-class (class-name (class-of obj)))
+         (index-name (get-objects-slot-index-name obj-class slot-symbol))
+         (index (get-root-object pool index-name)))
+    (when (and index (gethash (slot-value obj slot-symbol) index))
+      (remhash (%id  obj)
+               (gethash (slot-value obj slot-symbol) index)))))
+
+(defmethod tx-remove-objects-slot-index ((pool pool) class slot)
+  (let ((index-name (get-objects-slot-index-name class slot)))
+    (when (get-root-object pool index-name)
+      (remove-root-object pool index-name))))
+
+(defmethod drop-index-on ((pool pool) class &optional slots)
+  (dolist (slot slots)
+    (execute-transaction
+     (tx-remove-objects-slot-index pool class slot))))
--- src/managed-pool/pool.lisp
+++ src/managed-pool/pool.lisp
@@ -11,14 +11,6 @@
 ;;;
 ;;; 1. A convience macro
 ;;;
-(defmacro execute-transaction (transaction-call)
-  "Create a transaction object from transaction-call and execute it"
-  `(if (not (poolp ,(second transaction-call)))
-       (error "第一引数が pool ではありません。第一引数=~a" ,(second transaction-call))
-       (execute ,(second transaction-call)
-                (make-transaction ',(first transaction-call) ,@(rest (rest transaction-call))))))
-
-
 (defmethod get-preference ((pool pool) key)
   "Retrieve the value of the persistent preference stored under key in pool"
   (let ((preferences (get-root-object pool :preferences)))
@@ -30,13 +22,6 @@
   "Return the keyword symbol naming the root of instances of class"
   (let ((classname (if (symbolp class) (string class) (class-name class))))
     (intern (concatenate 'string classname "-ROOT") :keyword)))
-
-
-(defun get-objects-slot-index-name (class &optional (slot '%id))
-  "Return the keyword symbol naming the specified index of instances of class."
-  (let ((classname (if (symbolp class) (string class) (class-name class)))
-        (slotname  (symbol-name slot)))
-    (intern (concatenate 'string classname "-" slotname "-INDEX") :keyword)))
 
 
 (defmethod get-object-at-%id ((pool pool) class %id)
@@ -88,76 +73,6 @@
   (if slot
       (find-objects-with-slot pool class slot value test)
       (find-all-objects pool class)))
-
-
-(defmethod tx-create-objects-slot-index ((pool pool) class slot &optional (test #'equalp))
-  (let ((index-name (get-objects-slot-index-name class slot)))
-    (unless (get-root-object pool index-name)
-      (let ((index (make-hash-table :test test)))
-        (setf (get-root-object pool index-name) index)
-        (dolist (object (find-all-objects pool class))
-          (add-object-to-slot-index pool class slot object))))))
-
-
-(defmethod tx-remove-objects-slot-index ((pool pool) class slot)
-  (let ((index-name (get-objects-slot-index-name class slot)))
-    (when (get-root-object pool index-name)
-      (remove-root-object pool index-name))))
-
-
-(defun slot-index-xxx-add (index object slot)
-  (let ((%id-map (gethash (slot-value object slot) index))
-        (%id     (%id object)))
-    ;; 最初の時ね。
-    (when (null %id-map)
-      (setf %id-map   (make-hash-table))
-      (setf (gethash (slot-value object slot) index) %id-map))
-    ;; 既に存在するかチェックする。
-    ;; 存在する場合は何もしない。
-    (unless (gethash %id %id-map)
-      ;; 存在しない場合は追加する。
-      (setf (gethash %id %id-map) (%id object)))))
-
-
-(defmethod add-object-to-slot-index ((pool pool) class slot object)
-  (let* ((index-name (get-objects-slot-index-name class slot))
-         (index (get-root-object pool index-name)))
-    (when (and index  (slot-boundp object slot))
-      ;; 登録は実質こちらでやってます。
-      (slot-index-xxx-add index object slot))))
-
-
-(defun slot-index-xxx-remove (index object slot)
-  (let ((%id-map (gethash (slot-value object slot) index))
-        (%id     (%id object)))
-    ;; 既に存在するかチェックする。
-    ;; 存在する場合は %id-map から削除する。
-    (when %id-map ;;TODO: このケースって何じゃったっけ？
-      (when (gethash %id %id-map)
-        (remhash %id %id-map))
-      ;; %id-map が空になったら、index から削除する。
-      ;; TODO: これ、削除する必要あるの？
-      (when (= (hash-table-size %id-map) 0)
-        (remhash (slot-value object slot) index)))))
-
-
-(defmethod remove-object-from-slot-index ((pool pool) class slot object)
-  (let* ((index-name (get-objects-slot-index-name class slot))
-         (index (get-root-object pool index-name)))
-    (when (and index (slot-boundp object slot))
-      ;; 削除は実質こちらでやってます。
-      (slot-index-xxx-remove index object slot))))
-
-
-(defmethod index-on ((pool pool) class &optional slots (test 'equalp))
-  (dolist (slot slots)
-    (execute-transaction (tx-create-objects-slot-index pool class slot test))))
-
-
-(defmethod drop-index-on ((pool pool) class &optional slots)
-  (dolist (slot slots)
-    (execute-transaction (tx-remove-objects-slot-index pool class slot))))
-
 
 (defun slot-value-changed-p (object slot value)
   "Return true when slot in object is not eql to value (or when the slot was unbound)"
@@ -248,11 +163,6 @@
     (when (and index (gethash (slot-value obj slot-symbol) index))
       (remhash (%id  obj)
                (gethash (slot-value obj slot-symbol) index)))))
-
-
-(defun class-%id-indexp (symbol)
-  (cl-ppcre:scan "^(\\S)+-%ID-INDEX$"
-                 (symbol-name symbol)))
 
 
 (defun class-rootp (symbol)
