a0126e23bc6ce875dc5390ae642cc5eb33d828be
岩崎仁是 <yanqirenshi@gmail.com>
2016-04-02 12:34:36 +0900

    add with-pool

--- t/test-utility.lisp
+++ t/test-utility.lisp
@@ -1,9 +1,28 @@
 (defpackage :upanishad-test.test-utility
   (:use :cl :upanishad :prove :s-serialization)
-  (:export #:clear-pool-datastor))
+  (:export #:clear-pool-datastor
+           #:test-pool-directory
+           #:with-pool))
 (in-package :upanishad-test.test-utility)
 
 (defun clear-pool-datastor (directory)
   (when (probe-file directory)
     (dolist (pathname (directory (merge-pathnames "*.xml" directory)))
       (delete-file pathname))))
+
+
+(defvar *test-pool-root-directory* (pathname "/tmp/.upanishad/test/"))
+
+(defun test-pool-directory (target)
+  (merge-pathnames target *test-pool-root-directory*))
+
+
+(defmacro with-pool ((pool directory) &body body)
+  `(let ((,pool nil))
+     (unwind-protect
+          (clear-pool-datastor ,directory)
+       (setf ,pool (make-pool ,directory))
+       ,@body
+       (when pool
+         (stop pool)))))
+
