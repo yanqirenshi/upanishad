74c270986b2f7d7d888b1679be3827e23818e5c8
岩崎仁是 <yanqirenshi@gmail.com>
2016-04-02 17:00:16 +0900

    remove some comments

--- src/managed-pool/index-object.lisp
+++ src/managed-pool/index-object.lisp
@@ -52,33 +52,24 @@
 (defun %add-object-to-slot-index (index object slot)
   (let ((%id-map (gethash (slot-value object slot) index))
         (%id     (%id object)))
-    ;; 最初の時ね。
     (when (null %id-map)
       (setf %id-map   (make-hash-table))
       (setf (gethash (slot-value object slot) index) %id-map))
-    ;; 既に存在するかチェックする。
-    ;; 存在する場合は何もしない。
     (unless (gethash %id %id-map)
-      ;; 存在しない場合は追加する。
       (setf (gethash %id %id-map) (%id object)))))
 
 (defmethod add-object-to-slot-index ((pool pool) class slot object)
   (let* ((index-name (get-objects-slot-index-name class slot))
          (index (get-root-object pool index-name)))
     (when (and index  (slot-boundp object slot))
-      ;; 登録は実質こちらでやってます。
       (%add-object-to-slot-index index object slot))))
 
 (defun %remove-object-from-slot-index (index object slot)
   (let ((%id-map (gethash (slot-value object slot) index))
         (%id     (%id object)))
-    ;; 既に存在するかチェックする。
-    ;; 存在する場合は %id-map から削除する。
-    (when %id-map ;;TODO: このケースって何じゃったっけ？
+    (when %id-map
       (when (gethash %id %id-map)
         (remhash %id %id-map))
-      ;; %id-map が空になったら、index から削除する。
-      ;; TODO: これ、削除する必要あるの？
       (when (= (hash-table-size %id-map) 0)
         (remhash (slot-value object slot) index)))))
 
@@ -86,7 +77,6 @@
   (let* ((index-name (get-objects-slot-index-name class slot))
          (index (get-root-object pool index-name)))
     (when (and index (slot-boundp object slot))
-      ;; 削除は実質こちらでやってます。
       (%remove-object-from-slot-index index object slot))))
 
 (defmethod index-on ((pool pool) class &optional slots (test 'equalp))
