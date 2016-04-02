ef662ed2750d26902ab560c839d6bcb16516b7ee
岩崎仁是 <yanqirenshi@gmail.com>
2016-04-02 09:26:56 +0900

    :put_litter_in_its_place: #73

--- src/managed-pool/transaction.lisp
+++ src/managed-pool/transaction.lisp
@@ -6,5 +6,3 @@
        (error "第一引数が pool ではありません。第一引数=~a" ,(second transaction-call))
        (execute ,(second transaction-call)
                 (make-transaction ',(first transaction-call) ,@(rest (rest transaction-call))))))
-
-
