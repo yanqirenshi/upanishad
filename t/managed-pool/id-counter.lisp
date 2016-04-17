8dce2a0d7407d3d79a3085af1ef9f5cd2084d006
岩崎仁是 <yanqirenshi@gmail.com>
2016-04-17 18:32:39 +0900

    add test id-counter.lisp #73

--- t/managed-pool/id-counter.lisp
+++ t/managed-pool/id-counter.lisp
@@ -1,4 +1,4 @@
-(defpackage :upanishad-test.managed-pool.index
+(defpackage :upanishad-test.managed-pool.id-counter
   (:use :cl
         #:upanishad
         #:prove
@@ -9,5 +9,20 @@
 (defparameter *test-pool-directory* (test-pool-directory "managed-pool.id-counter"))
 
 (plan nil)
+
+(subtest "tx-create-%id-counter"
+  (with-pool (pool *test-pool-directory* :with-id-counter nil)
+
+    (ok (null (get-root-object pool :%id-counter)) "%id-counter is null")
+
+    (is (tx-create-%id-counter pool) 0 "success tx-create-%id-counter")
+
+    (ok (get-root-object pool :%id-counter) "can created %id-counter")))
+
+(subtest "next-%id"
+  (with-pool (pool *test-pool-directory*)
+    (is (get-root-object pool :%id-counter) 0 "first value is zero")
+    (is (next-%id pool) 1 "success next-%id")
+    (is (get-root-object pool :%id-counter) 1 "updated value is 1")))
 
 (finalize)
