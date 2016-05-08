4b565cc7a4c4a18030693017362695f4a5bac3c2
岩崎仁是 <yanqirenshi@gmail.com>
2016-05-08 10:04:35 +0900

    refactor #76

--- src/pool/basic.lisp
+++ src/pool/basic.lisp
@@ -132,17 +132,27 @@
       (close transaction-log-stream :abort abort)
       (setf transaction-log-stream nil))))
 
-(defmethod totally-destroy ((pool pool) &key abort)
-  "Totally destroy pool from permanent storage by deleting any files used by the pool, remove all root objects"
-  (close-open-streams pool :abort abort)
-  (when (probe-file (get-directory pool))
-    (dolist (pathname (directory (merge-pathnames (make-pathname :name :wild :type (file-extension pool))
-                                                  (get-directory pool))))
-      (delete-file pathname)))
-  (clrhash (root-objects pool)))
-
 (defmethod stop ((pool pool) &key abort)
   (close-open-streams pool :abort abort))
+
+(defun datastore-files (pool)
+  (directory (merge-pathnames (make-pathname :name :wild :type (file-extension pool))
+                              (get-directory pool))))
+
+(defmethod delete-all-files (pool)
+  (when (probe-file (get-directory pool))
+    (dolist (pathname (datastore-files pool))
+      (delete-file pathname))))
+
+(defmethod clear-objects (pool)
+  (clrhash (root-objects pool))
+  (clrhash (index-objects pool)))
+
+(defmethod totally-destroy ((pool pool) &key abort)
+  "Totally destroy pool from permanent storage by deleting any files used by the pool, remove all root objects"
+  (stop pool :abort abort)
+  (delete-all-files pool)
+  (clear-objects pool))
 
 
 ;;;
