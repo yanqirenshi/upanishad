358271c6e00a9538a534848bc1bdec761cfa4f55
yanqirenshi <yanqirenshi@gmail.com>
2016-08-14 14:28:19 +0900

    add test memes #87

--- t/managed-pool/memes.lisp
+++ t/managed-pool/memes.lisp
@@ -10,9 +10,30 @@
 
 (plan nil)
 
-(subtest "memes" (skip 1 "準備中"))
+(subtest "memes"
+  (let* ((contents '(1 2 3))
+         (class-symbol 'number)
+         (memes (make-instance 'memes
+                               :class-symbol class-symbol
+                               :contents contents)))
+    (ok memes "can make-isntace")
+    (is (class-symbol memes)
+        class-symbol
+        "can return class-symbol")
+    (is (contents memes)
+        contents
+        "can return contents")
+    (subtest "can return %id-ht"
+      (let ((%id-ht (%id-ht memes)))
+        (is (type-of %id-ht)
+            'hash-table "type is hash table")
+        (is (hash-table-count %id-ht)
+            0 "empty")))))
+
 (subtest "add-meme" (skip 1 "準備中"))
+
 (subtest "remove-meme" (skip 1 "準備中"))
+
 (subtest "make-memes" (skip 1 "準備中"))
 
 (finalize)
