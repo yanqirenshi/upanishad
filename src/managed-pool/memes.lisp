9f7ce041d00d07f86efbfeb19417d72fca466b82
yanqirenshi <yanqirenshi@gmail.com>
2016-08-27 12:59:41 +0900

    modify managed-pool/memes.lisp #92

--- src/managed-pool/memes.lisp
+++ src/managed-pool/memes.lisp
@@ -5,33 +5,33 @@
     :documentation ""
     :accessor class-symbol
     :initarg :class-symbol)
-   (contents :documentation ""
-             :accessor contents
-             :initarg :contents
-             :initform nil)
-   (%id-ht :documentation ""
-           :accessor %id-ht
-           :initarg :%id-ht
-           :initform (make-hash-table))))
+   (meme-list :documentation ""
+              :accessor meme-list
+              :initarg :meme-list
+              :initform nil)
+   (%id-index :documentation ""
+              :accessor %id-index
+              :initarg :%id-index
+              :initform (make-hash-table))))
 
 (defun get-meme (memes %id)
   (when (and memes %id)
-    (gethash %id (%id-ht memes))))
+    (gethash %id (%id-index memes))))
 
 (defgeneric add-meme (memes meme)
   (:method ((memes memes) (meme upanishad:meme))
     (let ((%id (up:%id meme)))
       (when (get-meme memes %id)
         (error "Aledy exist meme"))
-      (setf (gethash %id (%id-ht memes)) meme)
-      (push meme (contents memes)))
+      (setf (gethash %id (%id-index memes)) meme)
+      (push meme (meme-list memes)))
     memes))
 
-(defgeneric make-memes (class-symbol &key contents)
-  (:method ((object-symbol symbol) &key contents)
+(defgeneric make-memes (class-symbol &key meme-list)
+  (:method ((object-symbol symbol) &key meme-list)
     (let ((new-memes (make-instance 'memes
                                     :class-symbol object-symbol)))
-      (dolist (meme contents)
+      (dolist (meme meme-list)
         (add-meme new-memes meme))
       new-memes)))
 
@@ -40,7 +40,7 @@
     (let ((%id (up:%id meme)))
       (unless (get-meme memes %id)
         (error "Not exist meme"))
-      (remhash %id (%id-ht memes))
-      (setf (contents memes)
-            (remove meme (contents memes)))
+      (remhash %id (%id-index memes))
+      (setf (meme-list memes)
+            (remove meme (meme-list memes)))
       memes)))
