646fae81cb3a9acf09603562d88819b6ea91f574
yanqirenshi <yanqirenshi@gmail.com>
2016-08-27 12:38:19 +0900

    add file t/pool/basic.lisp #92

---
+++ t/pool/basic.lisp
@@ -0,0 +1,13 @@
+(defpackage :upanishad-test.pool
+  (:use #:cl
+        #:upanishad
+        #:prove
+        #:s-serialization
+        #:upanishad-test.test-utility))
+(in-package :upanishad-test.pool)
+
+(defparameter *test-pool-directory* (test-pool-directory "pool"))
+
+(plan nil)
+
+(finalize)
