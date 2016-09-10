05dde3f6c2ab540424fcafabf5a66bbc1f86472c
yanqirenshi <yanqirenshi@gmail.com>
2016-09-10 12:17:21 +0900

    split file at class.lisp #103

--- src/class/pool.lisp
+++ src/class/pool.lisp
@@ -1,57 +1,5 @@
-;;;;;
-;;;;; Defince Classes
-;;;;;
 (in-package :upanishad)
 
-
-;;;;;
-;;;;; supser class
-;;;;;
-;;; Brahman
-(defclass brahman () ()
-  (:documentation "思想的/象徴的なクラス。今んところ意味はないけぇ。"))
-
-;;; Atman
-(defclass atman (brahman)
-  ((%id :documentation "Return an external, unique, immutable identifier for object (typically an integer)"
-        :reader %id
-        :initarg :%id
-        :initform -1))
-  (:documentation "Superclass for objects with an id"))
-
-;;; Meme
-(defclass meme (atman)
-  ()
-  (:documentation "このクラスも思想的/象徴的なクラスです。まぁぶっちゃけ不要なんですけど。"))
-
-
-;;;;;
-;;;;; Blob
-;;;;;
-(defclass blob (meme)
-  ((name :documentation "Return the descriptive name of blob. Set the descriptive name of blob."
-         :accessor name
-         :initarg :name
-         :initform "untitled")
-   (size :documentation "Return the size of blob in bytes. Set the mime-type string of blob."
-         :reader size
-         :initarg :size
-         :initform -1)
-   (mime-type :documentation "Return the mime-type of blob as a string. Set the keywords list of blob"
-              :accessor mime-type
-              :initarg :mime-type
-              :initform "application/octet-stream")
-   (keywords :documentation "Return the list of keywords associated with blob"
-             :accessor keywords
-             :initarg :keywords
-             :initform '()))
-  (:documentation "A blob is a file-like collection of bytes with related metadata"))
-
-
-
-;;;;;
-;;;;; pool
-;;;;;
 (defclass pool (brahman)
   ((directory
     :documentation ":type pathname"
@@ -122,20 +70,3 @@
     :accessor guard
     :initform #'(lambda (thunk) (funcall thunk))))
   (:documentation "A Prevalence system with a guard thunk"))
-
-
-;;;;;
-;;;;; transaction
-;;;;;
-(defclass transaction (brahman)
-  ((args
-    :documentation ":type cons"
-    :accessor args
-    :initarg :args
-    :initform nil)
-   (function
-    :documentation ":type symbol"
-    :accessor get-function
-    :initarg :function
-    :initform 'identity))
-  (:documentation "A simple Transaction object joining a function and its arguments"))
