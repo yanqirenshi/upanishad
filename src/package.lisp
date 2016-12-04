f89d52bdbd8ec85d53cff66d8442a0ff8e94e055
yanqirenshi <yanqirenshi@gmail.com>
2016-12-05 00:24:10 +0900

    remove defpackage of s-serialization

--- src/package.lisp
+++ src/package.lisp
@@ -1,22 +1,4 @@
-;;;;;
-;;;;; Contents
-;;;;;  1. Define package
-;;;;;  2. Utilities
-;;;;;
-
-;;;
-;;; 1. Define package
-;;;
-(defpackage :s-serialization
-  (:use :cl)
-  (:export #:serializable-slots
-           #:serialize-xml
-           #:serialize-sexp
-           #:deserialize-xml
-           #:deserialize-sexp
-           #:make-serialization-state
-           #:reset-known-slots)
-  (:documentation "XML and s-expression based serialization for Common Lisp and CLOS"))
+(in-package :cl-user)
 
 (defpackage :upanishad.memes
   (:use :cl)
