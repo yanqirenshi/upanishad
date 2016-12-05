a7367b37860e1d479cdf38a977b704b6d22ee67c
yanqirenshi <yanqirenshi@gmail.com>
2016-12-05 10:48:45 +0900

    cosmetic changes #116

--- src/pool/snapshot-backup-restore.lisp
+++ src/pool/snapshot-backup-restore.lisp
@@ -1,5 +1,8 @@
 (in-package :upanishad)
 
+;;;
+;;; utility
+;;;
 (defun timetag (&optional (universal-time (get-universal-time)))
   (multiple-value-bind (second minute hour date month year)
       (decode-universal-time universal-time 0)
@@ -10,14 +13,20 @@
 ;;;
 ;;; file operator
 ;;;
+(defun tmp-pathname (file)
+  (merge-pathnames (concatenate 'string "tmp-" (pathname-name file)) file))
+
 (defun truncate-file (file position)
-  (let ((tmp-file (merge-pathnames (concatenate 'string "tmp-" (pathname-name file)) file))
+  (let ((tmp-file (tmp-pathname file))
         (buffer (make-string 4096))
         (index 0)
         (read-count 0))
     (with-open-file (in file :direction :input)
-      (with-open-file (out tmp-file :direction :output :if-exists :overwrite :if-does-not-exist :create)
-        (when (> position (file-length in)) (return-from truncate-file))
+      (with-open-file (out tmp-file :direction :output
+                                    :if-exists :overwrite
+                                    :if-does-not-exist :create)
+        (when (> position (file-length in))
+          (return-from truncate-file))
         (loop
           (when (= index position) (return))
           (setf read-count (read-sequence buffer in))
@@ -33,7 +42,9 @@
   (let ((buffer (make-string 4096))
         (read-count 0))
     (with-open-file (in source :direction :input)
-      (with-open-file (out target :direction :output :if-exists :overwrite :if-does-not-exist :create)
+      (with-open-file (out target :direction :output
+                                  :if-exists :overwrite
+                                  :if-does-not-exist :create)
         (loop
           (setf read-count (read-sequence buffer in))
           (write-sequence buffer out :end read-count)
@@ -43,9 +54,13 @@
 ;;;
 ;;; snapshot file
 ;;;
-(defvar *snapshot-types* '(:object :index :memes :indexes))
+(defvar *snapshot-types*
+  '(:object :index   ;; TODO: この二つは廃棄予定
+    :memes :indexes) ;;       新たにこの二つになります。
+  "スナップショットを取得する種類")
 
 (defun snapshot-type-p (type)
+  "type が snapshot-type かを返す。"
   (find type *snapshot-types*))
 
 (defmethod snapshot-pathnames (pool type)
@@ -59,6 +74,7 @@
     (setf (gethash type pathnames) value)))
 
 (defun make-snapshot-filename (type &optional suffix)
+  "snapshot のファイル名を返す。"
   (assert (snapshot-type-p type))
   (unless type (warn "type が nil ですよ。"))
   (if type
@@ -66,16 +82,21 @@
       (format nil "snapshot~@[-~a~]" suffix)))
 
 (defmethod make-snapshot-pathname (pool directory type &optional suffix)
-  (merge-pathnames (make-pathname :name (make-snapshot-filename type suffix)
-                                  :type (file-extension pool))
-                   directory))
+  "snapshot のファイル名をフルパスで返す。"
+  (let ((filename (make-snapshot-filename type suffix)))
+    (merge-pathnames (make-pathname :name filename
+                                    :type (file-extension pool))
+                     directory)))
 
 (defun make-snapshot-backup-pathname (pool directory type timetag)
+  "snapshot のバックアップ・ファイル名を返します。"
   (make-snapshot-pathname pool directory type timetag))
 
 (defun snapshot-copy-snapshot-file (pool directory type timetag)
+  ""
   (when (probe-file directory)
-    (copy-file directory (make-snapshot-backup-pathname pool directory type timetag))))
+    (copy-file directory
+               (make-snapshot-backup-pathname pool directory type timetag))))
 
 (defun backup-snapshot (snapshot snapshot-backup)
   (when (probe-file snapshot)
@@ -106,28 +127,43 @@
 ;;;
 ;;; snapshot
 ;;;
+(defun get-snapshot-objects (pool type)
+  "pool からシリアライズするオブジェクトを取得する。"
+  (cond ((eq type :object) (root-objects pool))
+        ((eq type :index) (index-objects pool))
+        ((eq type :memes) (memes pool))
+        ((eq type :indexes) (indexes pool))))
+
 (defun snapshot-objects (pool snapshot type)
+  "type で指定された Objects をシリアライズする。
+シリアライズしたものを snapshot に書き込む。"
   (assert (snapshot-type-p type))
-  (let ((objects (cond ((eq type :object) (root-objects pool))
-                       ((eq type :index) (index-objects pool)))))
-    (with-open-file (out snapshot :direction :output
+  (let ((objects (get-snapshot-objects pool type))
+        (serializer (serializer pool)))
+    (with-open-file (out snapshot :direction         :output
                                   :if-does-not-exist :create
-                                  :if-exists :supersede)
-      (funcall (serializer pool) objects out (serialization-state pool))))
+                                  :if-exists         :supersede)
+      (funcall serializer objects out (serialization-state pool))))
   pool)
 
 (defmethod snapshot ((pool pool))
   (let ((timetag (timetag))
         (transaction-log (transaction-log pool))
-        (snapshot-object (snapshot-pathnames pool :object))
-        (snapshot-index (snapshot-pathnames pool :index)))
+        (snapshot-object  (snapshot-pathnames pool :object))
+        (snapshot-index   (snapshot-pathnames pool :index))
+        (snapshot-memes   (snapshot-pathnames pool :memes))
+        (snapshot-indexes (snapshot-pathnames pool :indexes)))
     (close-open-streams pool)
     ;; backup snapshot
-    (snapshot-copy-snapshot-file pool snapshot-object :object timetag)
-    (snapshot-copy-snapshot-file pool snapshot-index :index timetag)
+    (snapshot-copy-snapshot-file pool snapshot-object  :object  timetag)
+    (snapshot-copy-snapshot-file pool snapshot-index   :index   timetag)
+    (snapshot-copy-snapshot-file pool snapshot-memes   :memes   timetag)
+    (snapshot-copy-snapshot-file pool snapshot-indexes :indexes timetag)
     ;; snapshot
-    (snapshot-objects pool snapshot-object :object)
-    (snapshot-objects pool snapshot-index :index)
+    (snapshot-objects pool snapshot-object  :object)
+    (snapshot-objects pool snapshot-index   :index)
+    (snapshot-objects pool snapshot-memes   :memes)
+    (snapshot-objects pool snapshot-indexes :indexes)
     ;; transaction log
     (snapshot-transaction-log pool transaction-log timetag)
     (when (fad:file-exists-p transaction-log)
@@ -147,11 +183,19 @@
          (snapshot-object-backup (make-snapshot-backup-pathname pool (or directory snapshot-object) :object timetag))
          ;; index
          (snapshot-index (snapshot-pathnames pool :index))
-         (snapshot-index-backup (make-snapshot-backup-pathname pool (or directory snapshot-index) :index timetag)))
+         (snapshot-index-backup (make-snapshot-backup-pathname pool (or directory snapshot-index) :index timetag))
+         ;; memes
+         (snapshot-memes (snapshot-pathnames pool :memes))
+         (snapshot-memes-backup (make-snapshot-backup-pathname pool (or directory snapshot-index) :memes timetag))
+         ;; indexes
+         (snapshot-indexes (snapshot-pathnames pool :indexes))
+         (snapshot-indexes-backup (make-snapshot-backup-pathname pool (or directory snapshot-index) :indexes timetag)))
     (close-open-streams pool)
     (backup-transaction-log transaction-log transaction-log-backup)
-    (backup-snapshot snapshot-object snapshot-object-backup)
-    (backup-snapshot snapshot-object snapshot-index-backup)
+    (backup-snapshot snapshot-object  snapshot-object-backup)
+    (backup-snapshot snapshot-index   snapshot-index-backup)
+    (backup-snapshot snapshot-memes   snapshot-memes-backup)
+    (backup-snapshot snapshot-indexes snapshot-indexes-backup)
     timetag))
 
 
@@ -169,6 +213,12 @@
                      (funcall deserializer in serialization-state)))
               ((eq type :index)
                (setf (index-objects pool)
+                     (funcall deserializer in serialization-state)))
+              ((eq type :memes)
+               (setf (memes pool)
+                     (funcall deserializer in serialization-state)))
+              ((eq type :indexes)
+               (setf (indexes pool)
                      (funcall deserializer in serialization-state)))))))
   pool)
 
@@ -196,5 +246,7 @@
   (close-open-streams pool)
   (restore-objects pool :object)
   (restore-objects pool :index)
+  (restore-objects pool :memes)
+  (restore-objects pool :indexes)
   (restore-transaction-log pool)
   pool)
