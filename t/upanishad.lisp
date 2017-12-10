b24fb3a6e1bcd336e29ed8d4c8e967709ec1c37b
yanqirenshi <yanqirenshi@gmail.com>
2017-12-10 09:35:25 +0900

    fix files and folders

--- t/upanishad.lisp
+++
@@ -1,9 +0,0 @@
-(defpackage :upanishad-test
-  (:use #:cl
-        #:upanishad
-        #:prove
-        #:s-serialization)
-  (:nicknames :up-test))
-(in-package :upanishad-test)
-
-(setf prove:*default-reporter* :dot)
