be507e8dd6a3d4acb65c58646df2b72f2fbd7b92
yanqirenshi <yanqirenshi@gmail.com>
2017-12-22 23:08:22 +0900

    update test

--- t/upanishad.lisp
+++ t/upanishad.lisp
@@ -2,13 +2,27 @@
 
 (defparameter *test-pool-directory* (test-pool-directory "demo"))
 
-(defclass test-meme (upanishad:meme) ())
+(defclass test-meme (upanishad:meme)
+  ((name :accessor name
+         :initarg :name
+         :initform "")))
 
-(plan nil)
+(plan 4)
 
 (with-pool (pool *test-pool-directory*)
   (subtest "create meme"
     (let ((meme (tx-create-meme pool 'test-meme)))
-      (ok meme "tx-create-meme"))))
+      (ok meme "tx-create-meme")))
+
+  (tx-create-meme pool 'test-meme)
+  (tx-create-meme pool 'test-meme)
+
+  (subtest "get-meme"
+    (ok (get-meme pool 'test-meme :%id 1))
+    (ok (get-meme pool 'test-meme :%id 2))
+    (ok (get-meme pool 'test-meme :%id 3)))
+
+  (subtest "find-meme"
+    (skip 1 "wait ....")))
 
 (finalize)
