81763911e848a22ae1d95d9ee3606169060bf7af
yanqirenshi <yanqirenshi@gmail.com>
2018-03-10 11:42:17 +0900

    rename dirs

---
+++ src/pool/perpetuation/package.lisp
@@ -0,0 +1,8 @@
+(in-package :cl-user)
+
+(defpackage :upanishad.pool.perpetuation
+  (:use #:cl
+        #:s-serialization)
+  (:nicknames :up.pool.perpetuation)
+  (:documentation ""))
+(in-package :upanishad.pool.perpetuation)
---
+++ src/pool/secretary/package.lisp
@@ -0,0 +1,8 @@
+(in-package :cl-user)
+
+(defpackage :upanishad.pool.secretary
+  (:use #:cl
+        #:s-serialization)
+  (:nicknames :up.pool.secretary)
+  (:documentation ""))
+(in-package :upanishad.pool.secretary)
